1f742a6937e11fca41dd269ae7fc2ba8ddaa64b6
bullinnyc <i8u11@icloud.com>
2021-06-20 09:55:42 +0700

    Initial Commit

---
+++ Presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Presentation
+//
+//  Created by Dmitry Kononchuk on 20.06.2021.
+//  Copyright © 2021 Dmitry Kononchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
